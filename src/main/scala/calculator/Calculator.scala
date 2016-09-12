289abc7c8abbe46f96e8854831703ea5b6130eb2
Vincenzo D'Amore <v.damore@gmail.com>
2016-09-12 13:22:44 +0200

    added method hasCyclicDependency

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import scala.collection.mutable.LinkedHashMap
+
 sealed abstract class Expr
 final case class Literal(v: Double) extends Expr
 final case class Ref(name: String) extends Expr
@@ -9,13 +11,73 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
+  
+  val evaluatedExpr: LinkedHashMap[String, List[String]] = LinkedHashMap()
+       
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
-  }
+       evaluatedExpr.clear()
+//       println("computeValues " + evaluatedExpr.size);
+       for {
+         (k,v) <- namedExpressions
+         } yield { 
+           val vVal = v()
+           val refNameList = evaluatedExpr.getOrElseUpdate(k, List())
+           (k -> Signal{ eval(k, vVal, namedExpressions) })
+         }
+     }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def hasCyclicDependency(lName : String, rName : String) : Boolean = {
+    println("check hasCyclicDependency " + lName + " " + rName);
+    def iter(l: String, r: String) : Boolean = {
+      if (l == r) {
+          println("found cyclic dependency " + l + " == " r)
+        true
+      } else {
+        val v = evaluatedExpr.find(p => { 
+//          println("looking for: " + l + " in key: " + p._1 ); 
+          p._2.contains(l) 
+        })
+        if (v.isEmpty) {
+//          println("cyclic dependency not found");
+          false
+        } else {
+//          println("found " + l + " into " + v.get._1 + " key ")
+          iter(v.get._1, rName)
+        }
+      }
+    }
+    iter(lName, rName)
+  }
+  
+  def eval(k: String, expr: Expr, references: Map[String, Signal[Expr]]): Double = {
+
+    expr match {
+           case l:Literal => { 
+//             println("Literal k: " + k + " value: " + l.v)
+             l.v 
+           }
+           case plus:Plus => eval(k, plus.a, references) + eval(k, plus.b, references)
+           case minus:Minus => eval(k, minus.a, references) - eval(k, minus.b, references)
+           case times:Times => eval(k, times.a, references) * eval(k, times.b, references)
+           case divide:Divide => eval(k, divide.a, references) * eval(k, divide.b, references)
+           case r:Ref => { 
+//            val ref = getReferenceExpr(r.name, references)
+//            eval(k, ref, references - r.name)
+
+             println("Ref k: " + k + " r.name: " +  r.name)
+             if (hasCyclicDependency(k, r.name)) {
+               Double.NaN
+             } else {
+               val expr = getReferenceExpr(r.name, references)
+               val refNameList = evaluatedExpr.get(k).get
+               if (!refNameList.contains(r.name))
+                 evaluatedExpr.put(k, r.name :: refNameList)
+               eval(k, getReferenceExpr(r.name, references), references)
+             }
+           }
+           case _ => { println("case not handled"); 0.0 }
+     }
   }
 
   /** Get the Expr for a referenced variables.
